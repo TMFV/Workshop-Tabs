3c0c6d598daa9499a680a61f1f302cfd5a02bc98
TMFV <68297974+TMFV@users.noreply.github.com>
2021-03-15 12:32:46 +0200

    code review

--- src/js/app.js
+++ src/js/app.js
@@ -3,28 +3,36 @@ const refs = {
     panes: document.querySelector('#tabs-1 [data-panes]'),
 };
 
-refs.controls.addEventListener('click', (event) => {
+refs.controls.addEventListener('click', onControlsClick(event) )
+
+function onControlsClick(event) {
+    {
     event.preventDefault();
     //console.log(event.target.nodeName);
     if (event.target.nodeName !== 'A') {
         console.log('Кликнули не в ссылку');
         return;
     }
-
     const controlItem = event.target;
     const currentActiveControlItem = refs.controls.querySelector('.controls__item--active');
     if (currentActiveControlItem) {
         currentActiveControlItem.classList.remove('controls__item--active');
-        const paneId = currentActiveControlItem.getAttribute('href').slice(1);
-        const pane = refs.panes.querySelector(`#${paneId}`);
+        const paneId = getPaneId(currentActiveControlItem)
+        const pane = getPaneById(paneId);
         pane.classList.remove('pane--active');
     }
     controlItem.classList.add('controls__item--active');
-    const paneId = controlItem.getAttribute('href').slice(1);
-    const pane = refs.panes.querySelector(`#${paneId}`);
-
+    const paneId = getPaneId(controlItem);
+    const pane = getPaneById(paneId);
     pane.classList.add('pane--active');
-    console.log(paneId);
-    console.log(pane);
+    //console.log(paneId);
+    //console.log(pane);
     //console.log(controlItem.getAttribute('href').slice(1));
-})
+}
+}
+function getPainId(control) {
+    return control.getAttribute('href').slice(1);
+}
+function getPaneById(id) {
+    return refs.panes.querySelector(`#${paneId}`);
+}
